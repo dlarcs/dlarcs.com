0a7244750e2fe320e179ae109794daea7fbc970b
Laura  Rozo <admin@promoflow.net>
2025-07-11 20:38:57 -0500

    Deleted all files

--- view/Managergym/view/global/footer/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cssTime = filemtime('../../view/global/footer/footer.css'); // ejemplo: '../Home/5.Video/video.css'
-$jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
-?>
-<link rel="stylesheet" href="../../view/global/footer/footer.css?v=<?= $cssTime ?>">
-<footer id="footer">
-  <strong>MANAGERGYM </strong> - @Copyright
-</footer>
-<script src="?v=<?= $jsTime ?>" type="text/javascript"></script>
